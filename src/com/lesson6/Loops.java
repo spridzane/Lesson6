7771ab27d91d45cf8f79da9e96f4b23da2366084
spridzane <zane.deksne@gmail.com>
2020-04-18 14:06:38 +0300

    new files added

---
+++ src/com/lesson6/Loops.java
@@ -0,0 +1,15 @@
+package com.lesson6;
+
+public class Loops {
+
+	public static void main(String[] args) {
+
+        //for loop example
+        int d;
+        //starting; cycle till i<10; adding +1 till <10
+        for(int i=5; i<10; i++){
+            System.out.println(i);
+        }
+    }//end main
+
+}//end class
